b5ce4e662794e138624ef59aea2b89298c3ea87c
Krish <krish@example.com>
2026-06-24 19:30:12 +0000

    refactor(backend): split server and app setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,30 @@
 import express from 'express';
 import prisma from './lib/prisma.js';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-  const user = prisma.user.findMany();
+app.use(cookieParser());
 
-  res.json({
-    message: 'Rentox is running',
-    user,
-  });
+app.get('/', async (req, res) => {
+  try {
+    const user = await prisma.user.findMany();
+
+    res.json({
+      message: 'Rentox is running',
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
+
+import authRouter from './routes/auth.routes.js';
+
+app.use('/api/v1/auth', authRouter);
 
 export default app;
